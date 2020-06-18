686c6fe477c72f1602a496e72b083ff24e800dc8
devenderbutani21 <devenderbutani21@gmail.com>
2020-06-18 11:49:28 +0530

    Made changes to UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,150 @@
-import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:file_picker/file_picker.dart';
+//import 'package:flutter/material.dart';
+//import 'package:audioplayers/audioplayers.dart';
+//import 'package:file_picker/file_picker.dart';
+//
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      debugShowCheckedModeBanner: false,
+//      title: 'Music Player',
+//      theme: ThemeData(
+//
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: HomePage(),
+//    );
+//  }
+//}
+//
+//class HomePage extends StatefulWidget {
+//  @override
+//  _HomePageState createState() => _HomePageState();
+//}
+//
+//class _HomePageState extends State<HomePage> {
+//  AudioPlayer _audioPlayer = AudioPlayer();
+//  bool isPlaying = false;
+//  String currentTime = "00:00";
+//  String completeTime= "00:00";
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//
+//    _audioPlayer.onAudioPositionChanged.listen((Duration duration){
+//      setState(() {
+//        currentTime = duration.toString().split(".")[0];
+//      });
+//    });
+//
+//    _audioPlayer.onDurationChanged.listen((Duration duration){
+//      setState(() {
+//        completeTime = duration.toString().split(".")[0];
+//      });
+//    });
+//
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    AssetImage imageAsset = AssetImage('assets/2.jpg');
+//    Image image = Image(image: imageAsset, fit: BoxFit.contain);
+//
+//    return Scaffold(
+//      backgroundColor: Colors.black,
+//      body: Stack(
+//        children: <Widget>[
+//          image,
+//
+//          Container(
+//            width: MediaQuery.of(context).size.width*0.8,
+//            height: 80,
+//
+//            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.7, left: MediaQuery.of(context).size.width*0.1),
+//
+//            decoration: BoxDecoration(
+//                color: Colors.white,
+//                borderRadius: BorderRadius.circular(50)
+//            ),
+//
+//
+//            child: Row(
+//              mainAxisAlignment: MainAxisAlignment.center,
+//              mainAxisSize: MainAxisSize.max,
+//              children: <Widget>[
+//                IconButton(
+//                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+//                  onPressed: (){
+//                    if(isPlaying){
+//                      _audioPlayer.pause();
+//
+//                      setState(() {
+//                        isPlaying = false;
+//                      });
+//                    }else{
+//                      _audioPlayer.resume();
+//
+//                      setState(() {
+//                        isPlaying = true;
+//                      });
+//                    }
+//
+//                  },
+//                )  ,
+//
+//                SizedBox(width: 16,),
+//
+//                IconButton(
+//                  icon: Icon(Icons.stop),
+//                  onPressed: (){
+//                    _audioPlayer.stop();
+//
+//                    setState(() {
+//                      isPlaying = false;
+//                    });
+//
+//                  },
+//                ),
+//
+//                Text(currentTime, style: TextStyle(fontWeight: FontWeight.w700),),
+//
+//                Text(" | "),
+//
+//                Text(completeTime, style: TextStyle(fontWeight: FontWeight.w300),),
+//
+//
+//
+//              ],
+//            ),
+//          ),
+//        ],
+//      ),
+//
+//
+//      floatingActionButton: FloatingActionButton(
+//        child: Icon(Icons.audiotrack),
+//        onPressed: () async{
+//          String filePath = await FilePicker.getFilePath();
+//
+//          int status = await _audioPlayer.play(filePath, isLocal: true);
+//
+//          if(status == 1){
+//            setState(() {
+//              isPlaying = true;
+//            });
+//          }
+//        },
+//      ),
+//    );
+//  }
+//}
 
+import 'package:flutter/material.dart';
+import 'screens/home/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,133 +155,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Music Player',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  AudioPlayer _audioPlayer = AudioPlayer();
-  bool isPlaying = false;
-  String currentTime = "00:00";
-  String completeTime= "00:00";
-
-  @override
-  void initState() {
-    super.initState();
-
-    _audioPlayer.onAudioPositionChanged.listen((Duration duration){
-      setState(() {
-        currentTime = duration.toString().split(".")[0];
-      });
-    });
-
-    _audioPlayer.onDurationChanged.listen((Duration duration){
-      setState(() {
-        completeTime = duration.toString().split(".")[0];
-      });
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    AssetImage imageAsset = AssetImage('assets/2.jpg');
-    Image image = Image(image: imageAsset, fit: BoxFit.contain);
-
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        children: <Widget>[
-          image,
-
-          Container(
-            width: MediaQuery.of(context).size.width*0.8,
-            height: 80,
-
-            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.7, left: MediaQuery.of(context).size.width*0.1),
-
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50)
-            ),
-
-
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                  onPressed: (){
-                    if(isPlaying){
-                      _audioPlayer.pause();
-
-                      setState(() {
-                        isPlaying = false;
-                      });
-                    }else{
-                      _audioPlayer.resume();
-
-                      setState(() {
-                        isPlaying = true;
-                      });
-                    }
-
-                  },
-                )  ,
-
-                SizedBox(width: 16,),
-
-                IconButton(
-                  icon: Icon(Icons.stop),
-                  onPressed: (){
-                    _audioPlayer.stop();
-
-                    setState(() {
-                      isPlaying = false;
-                    });
-
-                  },
-                ),
-
-                Text(currentTime, style: TextStyle(fontWeight: FontWeight.w700),),
-
-                Text(" | "),
-
-                Text(completeTime, style: TextStyle(fontWeight: FontWeight.w300),),
-
-
-
-              ],
-            ),
-          ),
-        ],
-      ),
-
-
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.audiotrack),
-        onPressed: () async{
-          String filePath = await FilePicker.getFilePath();
-
-          int status = await _audioPlayer.play(filePath, isLocal: true);
-
-          if(status == 1){
-            setState(() {
-              isPlaying = true;
-            });
-          }
-        },
-      ),
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,39 +8,167 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
+    const color = const Color(0xffFE6F5E);
+    const color1 = const Color(0xffFFF1D7);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Example File Structure"),
+//      backgroundColor: Colors.blueGrey.shade100,
+      backgroundColor: color,
+      body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.keyboard_arrow_down,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: () { },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                color: color1,
+                borderRadius: BorderRadius.circular(150),
+              ),
+              padding: EdgeInsets.all(6),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/2.jpg'),
+                maxRadius: 160,
+              ),
+            ),
+          ),
+          SizedBox(height: 30,),
+          Text(
+              "Song Name",
+              style: TextStyle(
+                  fontSize:20,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+          SizedBox(height: 8,),
+          Text(
+              "Artist Name",
+              style: TextStyle(
+                  fontSize:16,
+                  color: Colors.black45,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+          SizedBox(height: 30,),
+          Container(
+            padding: EdgeInsets.only(left: 20,right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("1.30",
+                    style: TextStyle(
+                    fontSize:13,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600
+                )),
+                Text("3.00",
+                    style: TextStyle(
+                        fontSize:13,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600
+                    )),
+              ],
+            ),
+          ),
+          Slider(
+            min: 0,
+            max: 3,
+            value: 1.5,
+          ),
+          SizedBox(height: 30,),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.shuffle,
+                    size: 20,
+                  ),
+                  onPressed: () { },
+                ),
+                SizedBox(width: 30,),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: color1,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                   child: IconButton(
+                    icon: Icon(
+                      Icons.skip_previous,
+                      size: 20,
+                      color: Colors.black,
+                      ),
+                     onPressed: () {},
+                    ),
+                ),
+                SizedBox(width: 18,),
+                Container(
+                  height: 75,
+                  width: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.pause,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                    onPressed: (){ },
+                  ),
+                ),
+                SizedBox(width: 18,),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: color1,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: IconButton(
+                  icon: Icon(
+                    Icons.skip_next,
+                    size: 20,
+                    color: Colors.black,
+                  ),
+                    onPressed: () { },
+                ),
+                ),
+                SizedBox(width: 30,),
+                IconButton(
+                  icon: Icon(
+                    Icons.repeat,
+                    size: 20,
+                  ),
+                  onPressed: () { },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 40),
-            ),
-            RaisedButton(
-              child: Text("increment"),
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-            ),
-            RaisedButton(
-              child: Text("decrement"),
-              onPressed: () {
-                setState(() {
-                  counter--;
-                });
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
