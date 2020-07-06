db94c00d4b3a01b2f710fb216c321122f75d3a9b
devenderbutani21 <devenderbutani21@gmail.com>
2020-07-06 23:21:21 +0530

    Circular Progress Bar Implemented

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,11 +11,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   AudioPlayer _audioPlayer = AudioPlayer();
   bool isPlaying = false;
   String currentTime = "00:00";
-  String completeTime= "00:00";
+  String completeTime = "00:00";
   double currentTimeSlider = 0.0;
   double completeTimeSlider = 0.0;
 
@@ -24,14 +22,14 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
 
-    _audioPlayer.onAudioPositionChanged.listen((Duration duration){
+    _audioPlayer.onAudioPositionChanged.listen((Duration duration) {
       setState(() {
         currentTime = duration.toString().split(".")[0];
         currentTimeSlider = duration.inSeconds.toDouble();
       });
     });
 
-    _audioPlayer.onDurationChanged.listen((Duration duration){
+    _audioPlayer.onDurationChanged.listen((Duration duration) {
       setState(() {
         completeTime = duration.toString().split(".")[0];
         completeTimeSlider = duration.inSeconds.toDouble();
@@ -44,11 +42,11 @@ class _HomeScreenState extends State<HomeScreen> {
 //    const color = const Color(0xffF5F5F5);
 //    const color1 = const Color(0xffFFF1D7);
 //    const color2 = const Color(0xffE34A27);
-    String filePath="";
+    String filePath = "";
     String songName = "Song Name";
-//    if(filePath!=" "){
-//      songName = filePath.split('/').last;
-//    }
+    if (filePath != "") {
+      songName = filePath.split('/').last;
+    }
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]); // For only allowing portrait view of the player
@@ -56,168 +54,37 @@ class _HomeScreenState extends State<HomeScreen> {
 //      backgroundColor: Colors.blueGrey.shade100,
 //      backgroundColor: Colors.black54,
       body: SafeArea(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                  Color.fromARGB(255, 25,178,238),
-                  Color.fromARGB(255, 21,236,229)
-              ]
-          )
-        ),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Colors.black,
-                      size: 30.0,
-                    ),
-                    onPressed: () async {
-                      filePath = await FilePicker.getFilePath();
-                      int status = await _audioPlayer.play(filePath, isLocal: true);
-
-                      if(status == 1) {
-                        setState(() {
-                          isPlaying = true;
-                        });
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Center(
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(150),
-                ),
-                padding: EdgeInsets.all(6),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/2.jpg'),
-                  maxRadius: 160,
-                ),
-              ),
-            ),
-//        SleekCircularSlider(
-//          min: 0,
-//          max: completeTimeSlider,
-//          initialValue: 0.0,
-//          onChange: (double value) {}
-//        ),
-            SizedBox(height: 30,),
-            Text(songName,
-                style: TextStyle(
-                    fontSize:20,
-                    fontWeight: FontWeight.w600
-                )
-            ),
-            SizedBox(height: 8,),
-            Text(
-                "Artist Name",
-                style: TextStyle(
-                    fontSize:16,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w600
-                )
-            ),
-            SizedBox(height: 30,),
-            Container(
-              padding: EdgeInsets.only(left: 20,right: 20),
-              child: Row(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color.fromARGB(255, 25, 178, 238),
+                Color.fromARGB(255, 21, 236, 229),
+//                    Color.fromARGB(255, 190,101,245),
+//                    Color.fromARGB(255, 218,120,221),
+//                    Color.fromARGB(255, 249,123,160),
+//                    Color.fromARGB(255, 250,112,137),
+              ])),
+          child: Column(
+            children: <Widget>[
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
                 children: <Widget>[
-                  Text(currentTime,
-                      style: TextStyle(
-                      fontSize:13,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600
-                  )),
-                  Text(completeTime,
-                      style: TextStyle(
-                          fontSize:13,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600
-                      )),
-                ],
-              ),
-            ),
-            Slider(
-              value: currentTimeSlider,
-              min: 0.0,
-              max: completeTimeSlider,
-              onChanged: (double value){
-                setState(() {
-                  _audioPlayer.seek(Duration(milliseconds: (value / 1000.0).toInt()));
-                  value = value;
-                });
-              },
-              activeColor: Color.fromARGB(255, 25,178,238),
-              inactiveColor: Colors.grey,
-            ),
-            SizedBox(height: 30,),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.shuffle,
-                      size: 20,
-                    ),
-                    onPressed: () { },
-                  ),
-                  SizedBox(width: 30,),
                   Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                     child: IconButton(
-                      icon: Icon(
-                        Icons.skip_previous,
-                        size: 20,
-                        color: Colors.black,
-                        ),
-                       onPressed: () {},
-                      ),
-                  ),
-                  SizedBox(width: 18,),
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 25,178,238),
-                      borderRadius: BorderRadius.circular(45),
-                    ),
                     child: IconButton(
-                      icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow,
-                          size: 30,
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
                         color: Colors.black,
+                        size: 30.0,
                       ),
-                      onPressed: (){
-                        if(isPlaying){
-                          _audioPlayer.pause();
+                      onPressed: () async {
+                        filePath = await FilePicker.getFilePath();
+                        int status =
+                            await _audioPlayer.play(filePath, isLocal: true);
 
-                          setState(() {
-                            isPlaying = false;
-                          });
-                        }else{
-                          _audioPlayer.resume();
-
+                        if (status == 1) {
                           setState(() {
                             isPlaying = true;
                           });
@@ -225,39 +92,191 @@ class _HomeScreenState extends State<HomeScreen> {
                       },
                     ),
                   ),
-                  SizedBox(width: 18,),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: IconButton(
-                    icon: Icon(
-                      Icons.skip_next,
-                      size: 20,
-                      color: Colors.black,
-                    ),
-                      onPressed: () { },
-                  ),
-                  ),
-                  SizedBox(width: 30,),
-                  IconButton(
-                    icon: Icon(
-                      Icons.repeat,
-                      size: 20,
-                    ),
-                    onPressed: () {
-//                    _audioPlayer.setReleaseMode(ReleaseMode.LOOP);
-                    },
-                  ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 300,
+                      width: 300,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 10.0,
+                        backgroundColor: Colors.grey,
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.blue),
+                        value: currentTimeSlider,
+                      ),
+                    ),
+                    Container(
+                      height: 300,
+                      width: 300,
+//                      decoration: BoxDecoration(
+//                        color: Colors.white,
+//                        borderRadius: BorderRadius.circular(150),
+//                      ),
+                      padding: EdgeInsets.all(6),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/2.jpg'),
+                        maxRadius: 160,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(songName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+              SizedBox(
+                height: 8,
+              ),
+              Text("Artist Name",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w600)),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(currentTime,
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600)),
+                    Text(completeTime,
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600)),
+                  ],
+                ),
+              ),
+//            Slider(
+//              value: currentTimeSlider ?? 0,
+//              min: 0.0,
+//              max: completeTimeSlider,
+//              onChanged: (value){
+//                setState(() {
+//                  _audioPlayer.seek(Duration(milliseconds: (value / 1000.0).toInt()));
+//                  value = currentTimeSlider;
+//                });
+//              },
+//              activeColor: Color.fromARGB(255, 25,178,238),
+//              inactiveColor: Colors.grey,
+//            ),
+//              SizedBox(
+//                height: 30,
+//              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.shuffle,
+                        size: 20,
+                      ),
+                      onPressed: () {},
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.skip_previous,
+                          size: 20,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 18,
+                    ),
+                    Container(
+                      height: 75,
+                      width: 75,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 25, 178, 238),
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          isPlaying ? Icons.pause : Icons.play_arrow,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          if (isPlaying) {
+                            _audioPlayer.pause();
+
+                            setState(() {
+                              isPlaying = false;
+                            });
+                          } else {
+                            _audioPlayer.resume();
+
+                            setState(() {
+                              isPlaying = true;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 18,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.skip_next,
+                          size: 20,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.repeat,
+                        size: 20,
+                      ),
+                      onPressed: () {
+//                    _audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:audio_playere_tet/main.dart';
+import 'package:musicplayerflutterapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
