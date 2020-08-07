2a75d1a47f00911ae53d4315596e198d38f26ebb
devenderbutani21 <devenderbutani21@gmail.com>
2020-08-07 21:36:40 +0530

    Old Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'screens/home.dart';
-import 'screens/music_player.dart';
+import 'screens/home.dart';
+//import 'screens/music_player.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MusicPlayer(),
+      home: HomeScreen(),
     );
   }
 }
