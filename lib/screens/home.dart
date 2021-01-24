c923447674bec2342248d151c4616babdc455fe0
devenderbutani21 <devenderbutani21@gmail.com>
2021-01-24 18:00:51 -0500

    Bottom Navigation Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,11 +7,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _bottomNavIndex = 0; //default index of first screen
+
+  AnimationController _animationController;
+  Animation<double> animation;
+  CurvedAnimation curve;
+
+  final iconList = <IconData>[
+    Icons.home,
+    Icons.person_outline,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Hello World'),
+        child: Text(
+          'Hello World',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.music_note),
+        backgroundColor: Color(0xff3E2AD1),
+        onPressed: () {
+
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
       ),
     );
   }
